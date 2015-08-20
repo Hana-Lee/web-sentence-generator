a5443ec25c90c3e531e34d005cb2f038d7cde0e5
Hana Lee <i@leehana.co.kr>
2015-08-21 04:55:17 +0900

    webix 라이브러리 한국어 지원 i18n 코드 추가

---
+++ web/js/webix/codebase/i18n/ko.js
@@ -0,0 +1,35 @@
+webix.i18n.locales["ko-KR"]={
+	groupDelimiter:",",
+	groupSize:3,
+	decimalDelimiter:".",
+	decimalSize:2,
+
+	dateFormat:"%Y-%m-%d",
+	timeFormat:"%H:%i",
+	longDateFormat:"%Y년 %m월 %d일",
+	fullDateFormat:"%Y-%m-%d %H:%i",
+
+	price:"₩{obj}",
+	priceSettings:{
+		groupSize:3,
+		groupDelimiter:",",
+		decimalDelimiter:"",
+		decimalSize:0
+	},
+
+	calendar:{
+		monthFull:["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+		monthShort:[ "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+		dayFull:["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
+		dayShort:["일", "월", "화", "수", "목", "금", "토"],
+		hours: "시",
+		minutes: "분",
+		done: "완료",
+		clear: "삭제",
+		today: "금일"
+	},
+
+	controls:{
+		select:"선택"
+	}
+};
